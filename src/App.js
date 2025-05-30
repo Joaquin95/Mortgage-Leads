5a51cbae8faaa00fcabfd5950c84ced692de517e
Joaquin <Mintinvestments95@gmail.com>
2025-05-30 16:20:09 -0500

    added stripe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./components/Dashboard";
 import LoginForm from "./components/LoginForm";
+import Leadform from "./components/LeadForm";
+import AdminPanel from "./components/AdminPanel";
+// import OfficerSignup from "./components/OfficerSignup";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leadform" element={<Leadform />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        {/* <Route path="/signup" element={<OfficerSignup />} /> */}
       </Routes>
     </Router>
   );
