ef7d4a9dbd580d7ddd888204b3576c1f35fe4777
Joaquin <Mintinvestments95@gmail.com>
2025-07-21 13:38:01 -0500

    add triggers to officers 2

--- functions/officerTriggers.js
+++ functions/officerTriggers.js
@@ -1,15 +1,13 @@
-const functions    = require("firebase-functions");
-const admin        = require("./initAdmin");
+const functions = require("firebase-functions");
+const admin = require("./initAdmin");
 const TOTAL_SHARDS = 10;
 
-exports.onOfficerCreate = functions
-  .auth
-  .user()
-  .onCreate(async (user) => {
-    console.log("New officer signup:", user.uid);
-    return admin
-      .firestore()
-      .collection("loanOfficers")
-      .doc(user.uid)
-      .set({ shardTest: true });
-  });
+exports.onOfficerCreate = functions.auth.user().onCreate(async (user) => {
+  console.log("🔥 onOfficerCreate fired for uid:", user.uid);
+
+  return admin
+    .firestore()
+    .collection("loanOfficers")
+    .doc(user.uid)
+    .set({ shardTest: true });
+});
